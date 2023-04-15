3a3b5ff967f2fce6af6ced6f5d90e14427534266
eunji.sim <123479997+planted-ji@users.noreply.github.com>
2023-04-15 23:56:58 +0900

    두 수의 합, 두 수의 차, 두 수의 곱, 몫 구하기

---
+++ programmers/level-0/001.js
@@ -0,0 +1,23 @@
+//두 수의 합
+function solution(num1, num2) {
+  const answer = num1 + num2;
+  return answer;
+}
+
+// 두 수의 차
+function solution(num1, num2) {
+  const answer = num1 - num2;
+  return answer;
+}
+
+// 두 수의 곱
+function solution(num1, num2) {
+  const answer = num1 * num2;
+  return answer;
+}
+
+// 몫 구하기
+function solution(num1, num2) {
+  const answer = parseInt(num1 / num2);
+  return answer;
+}
