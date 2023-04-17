401288722a80a26f26ca32401f07814fe2819ca3
eunji.sim <123479997+planted-ji@users.noreply.github.com>
2023-04-17 23:53:17 +0900

    ✏ '숫자 비교하기' 풀이 추가

--- programmers/level-0/002.js
+++ programmers/level-0/002.js
@@ -3,3 +3,9 @@ function solution(num1, num2) {
   const answer = parseInt((num1 / num2) * 1000);
   return answer;
 }
+
+// 숫자 비교하기
+function solution(num1, num2) {
+    let answer;
+    return answer = (num1 == num2) ? 1 : -1;
+}
