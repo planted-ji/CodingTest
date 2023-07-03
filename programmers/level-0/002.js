730c3cb8426b0b6fa0ead3c38be9be96a12ec345
eunji.sim <123479997+planted-ji@users.noreply.github.com>
2023-07-04 01:35:33 +0900

    분수의 덧셈 풀이방법 업데이트

--- programmers/level-0/002.js
+++ programmers/level-0/002.js
@@ -1,42 +1,32 @@
-// 두 수의 나눗셈
+// * 두 수의 나눗셈
 function solution(num1, num2) {
   const answer = parseInt((num1 / num2) * 1000);
   return answer;
 }
 
-// 숫자 비교하기
+// * 숫자 비교하기
 function solution(num1, num2) {
     let answer;
     return answer = (num1 == num2) ? 1 : -1;
 }
 
-// 분수의 덧셈
-function solution(numer1, denom1, numer2, denom2) {
-    const answer = [];
-    const hapNumer = numer1 * denom2 + numer2 * denom1
-    const hapDenom = denom1 * denom2
-    
-    let minNumber;
-    if(hapNumer < hapDenom) {
-        minNumber = hapNumer;
-    } else {
-        minNumber = hapDenom;
-    }
-    
-    while(true){
-        if(hapNumer % minNumber === 0) {
-            if(hapDenom % minNumber === 0) {
-                return [hapNumer / minNumber, hapDenom / minNumber]
-            }
-        }
-        minNumber = minNumber - 1;
-    }
-    return answer;
+// * 분수의 덧셈
+function minNumber(a, b) {
+  // 유클리드 알고리즘 사용
+  if (b === 0) {
+    return a;
+  }
+  return minNumber(b, a % b);
 }
 
-// 1. 분모 덧셈
-// 2. 분자분모의 최대공약소로 나누기
-// 2-1. 분자분모 중 작은 수 찾기
-// 2-2. 작은 수를 분자분모로 나누기
-// 2-2-1. 나누어 떨어지면 나눈 수가 최대공약수
-// 2-2-2. 나누어 떨이지지 않으면 작은 수를 1 줄이고 2-2로 돌아가기
+function solution(numer1, denom1, numer2, denom2) {
+    const hapNumer = numer1 * denom2 + numer2 * denom1
+    const hapDenom = denom1 * denom2
+
+  // 분자분모의 최대공약수로 나누기
+  const minValue = minNumber(hapNumer, hapDenom);
+  const numerValue = hapNumer / minValue;
+  const denomValue = hapDenom / minValue;
+
+  return [numerValue, denomValue];
+}
